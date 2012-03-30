615c2c95156102e7c05f995ece75772b2711c1a5
Kicool <kicool@foxmail.com>
2012-03-30 13:31:51 +0800

    using GAE's Users service, upload to GAE

--- helloapp/src/hello.go
+++ helloapp/src/hello.go
@@ -1,14 +1,33 @@
 package hello
 
 import (
+	"appengine"
+	"appengine/user"
 	"fmt"
 	"net/http"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/login", handlerLogin)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+	fmt.Fprint(w, "Hello, world")
+}
+
+func handlerLogin(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello, %v!", u)
 }
